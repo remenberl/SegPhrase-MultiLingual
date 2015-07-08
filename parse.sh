9e1e2a5669deac21ae0d7553fca557d183edd41c
Jialu Liu <jliu64@dmserv4.cs.illinois.edu>
2015-07-07 22:36:22 -0500

    Change echo

--- parse.sh
+++ parse.sh
@@ -4,8 +4,9 @@ export RAW_TEXT=data/test.txt
 opencc -i ${RAW_TEXT} -o tmp/pre_test.txt -c tw2s.json
 ${PYPY} src/preprocessing/punctuation.py -i tmp/pre_test.txt -o tmp/test.txt
 ${PYPY} src/online_query/encoding.py -i tmp/test.txt -o tmp/test.token -map results/mapping.txt -offset tmp/offset.txt
-./bin/segphrase_parser results/segmentation.model results/salient.csv 5000 tmp/test.token tmp/test.parsed.txt 0
 echo parsing ...
+./bin/segphrase_parser results/segmentation.model results/salient.csv 5000 tmp/test.token tmp/test.parsed.txt 0
+echo decoding ...
 ${PYPY} src/online_query/decoding.py -i tmp/test.parsed.txt -o tmp/test.result -map results/mapping.txt -orig ${RAW_TEXT} -offset tmp/offset.txt
 
 ${PYPY} src/online_query/compute_offset.py tmp/test.result results/offset.txt
